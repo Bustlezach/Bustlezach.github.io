21a385c948adf1943fdc00642da09e4aba4d1101
OLUWASEUN OLORUNSHOLA <bustlezach001@gmail.com>
2024-04-29 17:15:05 +0100

    added react lazy to the portfolio and contactme components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import NavBar from "./components/NavBar";
 import Intro from "./components/Intro";
 import About from "./components/About";
-import Portfolio from "./components/Portfolio";
-// import Clients from "./components/Clients";
 import Contactme from "./components/Contactme";
 import Footer from "./components/Footer";
+const LazyPortfolio = lazy(() => import("./components/Portfolio"))
+const LazyContactme = lazy(() => import("./components/Portfolio"))
 
 
 function App() {
+
+
   return (
     <div className="App">
       <NavBar />
       <Intro />
       <About />
-      <Portfolio />
+      <Suspense fallback={<div>Loading...</div>}>
+        <LazyPortfolio />
+        <LazyContactme />
+      </Suspense>
       {/* <Clients /> */}
       <Contactme />
       <Footer />
